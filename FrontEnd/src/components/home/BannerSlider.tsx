522aecb790f439a43a7117f4cbf1ae1f6de47c4b
Harikrishnan G <harikrishnangmonkuttan@gmail.com>
2026-04-23 17:22:23 +0530

    Improve-performance-pages-2

--- FrontEnd/src/components/home/BannerSlider.tsx
+++ FrontEnd/src/components/home/BannerSlider.tsx
@@ -93,6 +93,7 @@ export default function BannerSlider() {
                                 loop
                                 muted
                                 playsInline
+                                crossOrigin="anonymous"
                             />
                         ) : (
                             <Image
